6e19deefc6c52d5ecddfdb2efa3525d1962e8756
Somaiyah Hafeez <sh4625@columbia.edu>
2025-05-16 22:17:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false, 
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'Somaiyah Hafeez is a data journalist at Columbia Journalism School. <br> Find the code for this project <a href="https://github.com/somaiyahhafeez/pahalgam"> here.</a> <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
 
         {
